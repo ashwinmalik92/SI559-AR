3d36b31f69cec2bc9c5dc2c3b4e008836a8dc751
Ashwin Malik <ashwinmalik92@gmail.com>
2020-11-05 23:53:01 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,14 +3,13 @@ window.onload = function () {
         let disp = document.querySelector("#display");
         var images = ["viz1", "viz2", "viz3"];
         var current = disp.getAttribute("src");
-        var currentIndex = images.indexOf(current);
-        var currentIndex = currentIndex + 1;
-        if (newIndex >= images.length) {
-            newIndex = 0;
+        var index = images.indexOf(current) + 1;
+        if (index >= images.length) {
+            index = 0;
         }
         console.log(current);
         console.log(currentIndex);
-        console.log(newIndex);
-        disp.setAttribute("src", newIndex);
+        console.log(index);
+        //disp.setAttribute("src", index);
     });
 };
