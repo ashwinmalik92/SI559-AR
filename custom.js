8c4a1edb0d86304e5c1ea36eeb377b4bdd3b7d25
Ashwin Malik <ashwinmalik92@gmail.com>
2020-11-05 23:48:37 -0500

    size

--- custom.js
+++ custom.js
@@ -2,12 +2,12 @@ window.onload = function () {
     document.querySelector(".img-switch").addEventListener("click", function () {
         let disp = document.querySelector("#display");
         var images = ["viz1", "viz2", "viz3"];
-        var current = disp.getAttribute("src");
-        var currentIndex = images.indexOf(current);
-        var newIndex = currentIndex + 1;
+        var current = disp.getAttribute("src")
+        var currentIndex = images.indexOf(current)
+        var newIndex = currentIndex + 1
         if (newIndex >= images.length) {
             newIndex = 0;
         }
-        document.querySelector("#display").setAttribute("src", newIndex);
+        disp.setAttribute("src", newIndex);
     });
 };
