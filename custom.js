080a1993511733583121a5c917260b8a5b7c0b4d
Ashwin Malik <ashwinmalik92@gmail.com>
2020-11-05 23:55:02 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,15 +1,15 @@
 window.onload = function () {
     document.querySelector(".img-switch").addEventListener("click", function () {
         let disp = document.querySelector("#display");
-        var images = ["viz1", "viz2", "viz3"];
+        var images = ["#viz1", "#viz2", "#viz3"];
         var current = disp.getAttribute("src");
         var index = images.indexOf(current) + 1;
         if (index >= images.length) {
             index = 0;
         }
         console.log(current);
-        console.log(currentIndex);
         console.log(index);
+        console.log(images[index])
         //disp.setAttribute("src", index);
     });
 };
