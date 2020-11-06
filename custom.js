69595180f110f11722fafadf80ce01599b1517bb
Ashwin Malik <ashwinmalik92@gmail.com>
2020-11-05 22:41:15 -0500

    button

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    document.querySelector(".say-hi-button").addEventListener("click", function () {
+    document.querySelector(".img-switch").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
